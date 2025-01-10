968356c4e049d2bb931cabc9cf2a3c89307c6731
AnnaZuzkova <famzu2018@gmail.com>
2025-01-10 19:36:56 +0200

    виправила помилки, переставила export

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,10 @@ const form = document.querySelector("#form");
 const userName = document.querySelector(".user");
 const email = document.querySelector(".email");
 const comment = document.querySelector(".comment");
-const submit = document.querySelector(".submit");
 
 const formMessage = (event) => {
   event.preventDefault();
-  userHandler(userName.ariaValueMax, email.ariaValueMax, comment.value);
-  alert("done");
+  userHandler(userName.value, email.value, comment.value);
+  alert("Дані успішно відправлені!");
 };
 form.addEventListener("submit", formMessage);
--- src/js/userobject.js
+++ src/js/userobject.js
@@ -1,8 +1,9 @@
-export { userHandler };
 const userHandler = (nameUser, emailUser, commentUser) => {
   const user = {
     name: nameUser,
     email: emailUser,
     comment: commentUser,
   };
+  return user;
 };
+export { userHandler };
